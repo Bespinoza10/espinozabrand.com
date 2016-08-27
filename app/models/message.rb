16d54524085095a34d0a8c79bb13bdb41ffa5fa1
Brandon Espinoza <espinozab100@gmail.com>
2016-08-26 21:13:53 -0500

    Made table-less Message model with attrs and testing

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,5 @@
 class Message
+  include ActiveModel::Model
+  attr_accessor :name, :email, :number, :content
+  validates :name, :email, :number, :content, presence: true
 end
--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class MessageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'responds to name, number, email and content' do
+    msg = Message.new
+    [:name, :number, :email, :content].each do |attr|
+      assert msg.respond_to? attr
+    end
+  end
+
+  test 'should accept valid attributes' do
+    valid_attrs = {
+      name: 'brandon',
+      email: 'brandon@example.com',
+      number: '111-111-1111',
+      content: 'qwerty'
+    }
+
+    msg = Message.new valid_attrs
+
+    assert msg.valid?
+  end
+
+  test 'attributes can not be blank' do
+    msg = Message.new
+    refute msg.valid?
+  end
 end
