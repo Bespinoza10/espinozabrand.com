4a14187b4dbadc7005e1d5e5a07499e09ce10fd7
Brandon Espinoza <espinozab100@gmail.com>
2016-09-01 22:17:44 -0500

    project CRUD ability implemented. Currently form works to add new projects. Styling WIP

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,52 @@
 class PortfoliosController < ApplicationController
+
+  def index
+    @portfolio = Portfolio.all.order("created_at desc")
+  end
+
+  def new
+    @portfolio = Portfolio.new
+  end
+
+  def create
+    @portfolio = Portfolio.new portfolio_params
+
+    if @portfolio.save
+      redirect_to @portfolio, notice: "Awesome Brandon! That portfolio was succesfully saved!"
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @portfolio.update portfolio_params
+      redirect_to @portfolio, notice: "Ahhww Yea!!! Your portfolio was saved succesfully!"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @portfolio.destroy
+    redirect_to portfolios_path
+  end
+
+  private
+
+  def find_portfolio
+    # @portfolio = portfolio.friendly.find(params[:id])
+    @portfolio = portfolio.find(params[:id])
+  end
+
+  def portfolio_params
+    params.require(:portfolio).permit(:name, :description, :link, :slug)
+  end
+
 end
