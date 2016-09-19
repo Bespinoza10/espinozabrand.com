2084436965511cc7aaaf28643ded69bc6dda95ea
Brandon Espinoza <espinozab100@gmail.com>
2016-09-19 10:53:49 -0500

    Instaled jquery plug in that gets the main color of an image and displays it as a background image. Cleaning code WIP

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,5 +1,4 @@
 class Portfolio < ActiveRecord::Base
-  require 'rmagick'
 
   has_attached_file :image_main, styles: { large: "800x800", medium: "500x500>", thumb: "200x200>" }
   has_attached_file :web_image, styles: { large: "800x800", medium: "500x500>", thumb: "200x200>" }
@@ -9,13 +8,5 @@ class Portfolio < ActiveRecord::Base
   validates_attachment_content_type :web_image, content_type: /\Aimage\/.*\z/
   validates_attachment_content_type :logo_image, content_type: /\Aimage\/.*\z/
   validates_attachment_content_type :bc_image, content_type: /\Aimage\/.*\z/
-
-   before_create :dominant_color
-
-  def dominant_color(image)
-    img = Magick::Image.read(image).first
-    color = img.scale(1, 1).pixel_color(0, 0)
-    img.to_color(color)
-  end
 
 end
